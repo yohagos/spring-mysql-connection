68d4dc428b1265555c06ed0182344062a7ae8c95
yohagos <yosef.hagos@googlemail.com>
2023-10-20 14:57:11 +0200

    init project

---
+++ src/test/java/com/dataaccessmysql/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataaccessmysql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
